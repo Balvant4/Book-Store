8e60b216b83852a813fe2423cc9ca5857ef48b96
Balvant4 <balvantkushwaha44@gmail.com>
2024-07-09 14:40:46 +0530

    Making one route using cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,19 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// routes import
+import userRouter from "../src/routes/user.routes.js";
+
+// route declaration
+app.use("api/v1/users", userRouter);
 
 export { app };
---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+
+const router = Router();
+
+//router.route("/register").post(registerroute) example not including in code
+export default router;
